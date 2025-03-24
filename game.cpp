240398ac92961e374bb242f2daeec08bcb77e1ec
Francisco <franciscoabreu@gmail.com>
2025-03-24 15:39:30 -0300

    Setting up Perspective enviroment

--- game.cpp
+++ game.cpp
@@ -28,16 +28,16 @@ void Game::Init()
     ResourceManager::LoadShader("Shaders/shaderCoordinate.vs", "Shaders/shaderCoordinate.fs", nullptr, "coordinate");
 
    //configure coordinate system
-    //glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(this->Width) / static_cast<float>(this->Height), 0.1f, 100.0f);
-    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width), 
-        static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
-    //glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(this->Width) / static_cast<float>(this->Height), 0.1f, 100.0f);
+   // glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width), 
+    //    static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+    glm::mat4 view = glm::mat4(1.0f);
     // note that we're translating the scene in the reverse direction of where we want to move
-   // view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
     ResourceManager::GetShader("coordinate").Use().SetInt("image", 0);
     ResourceManager::GetShader("coordinate").SetMat4("projection", projection);
-   // ResourceManager::GetShader("coordinate").SetMat4("view", view);
+    ResourceManager::GetShader("coordinate").SetMat4("view", view);
 
     // set render-specific controls
     Shader coordinateShader = ResourceManager::GetShader("coordinate");
--- spriteRenderer.cpp
+++ spriteRenderer.cpp
@@ -30,14 +30,11 @@ void SpriteRenderer::initRenderData()
     float vertices[] = { 
     // pos
 
-    // pos      // tex
-    0.0f, 1.0f, 0.0f, 1.0f,
-    1.0f, 0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 0.0f, 0.0f, 
-
-    0.0f, 1.0f, 0.0f, 1.0f,
-    1.0f, 1.0f, 1.0f, 1.0f,
-    1.0f, 0.0f, 1.0f, 0.0f
+    // positions          // texture coords
+    0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+    0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+   -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
     
     /*
     // first triangle
@@ -53,13 +50,20 @@ void SpriteRenderer::initRenderData()
 
     glGenVertexArrays(1, &this->quadVAO);
     glGenBuffers(1, &VBO);
+
+    glBindVertexArray(this->quadVAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindVertexArray(this->quadVAO);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);  
     glBindVertexArray(0);
 }
@@ -75,13 +79,15 @@ glm::vec2 size, float rotate, glm::vec3 color)
       // prepare transformations
       this->shader.Use();
       glm::mat4 model = glm::mat4(1.0f);
-      model = glm::translate(model, glm::vec3(position, 0.0f)); 
-
+     // model = glm::translate(model, glm::vec3(position, 0.0f)); 
+      model = glm::rotate(model, glm::radians(rotate), glm::vec3(1.0f, 0.0f, 0.0f)); 
+      /*
       model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f)); 
       model = glm::rotate(model, glm::radians(rotate), glm::vec3(0.0f, 0.0f, 1.0f)); 
       model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f));
-
-      model = glm::scale(model, glm::vec3(size, 1.0f)); 
+      */
+      
+      //model = glm::scale(model, glm::vec3(size, 1.0f)); 
     
       this->shader.SetMat4("model", model);
       this->shader.SetVec3("spriteColor", color);
